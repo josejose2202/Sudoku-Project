b030929e8943c8b3e01868785d2a4a6c6c2f90eb
alexlin11 <116769028+alexlin11@users.noreply.github.com>
2022-12-01 19:20:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ class SudokuGenerator:
     def __init__(self, row_length=9, removed_cells= 30):
         self.row_length = row_length
         self.remove_cells = removed_cells
+        self.board = []
         self.box_length = math.sqrt(row_length)
     def get_board(self):
-        return [["-" for i in range(self.row_length)] for j in range(9)]
+        return [[0 for i in range(self.row_length)] for j in range(9)]
 
     def print_board(self):
-        for row in self.get_board():
+        self.board = self.get_board()
+        for row in self.board:  # row: ["-", "-", "-"]
             for col in row:
                 print(col, end=" ")
             print()
